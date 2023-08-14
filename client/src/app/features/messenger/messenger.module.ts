1d619f7aa6f427af6d71de6e53323e40e0be2d2f
s2000promax <s2000promax@gmail.com>
2023-08-14 22:43:55 +0400

    feat: main layout

---
+++ client/src/app/features/messenger/messenger.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessengerComponent } from './messenger.component';
+
+@NgModule({
+    declarations: [MessengerComponent],
+    imports: [CommonModule],
+    exports: [MessengerComponent],
+})
+export class MessengerModule {
+}
