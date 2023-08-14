6f9405eb671799b9a10cb2986b4537a885a4c665
s2000promax <s2000promax@gmail.com>
2023-08-14 21:35:12 +0400

    feat: client environment prepare

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://itransition-chat-server.vercel.app/api',
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://itransition-chat-server.vercel.app/api',
+};
